bdb21271575dcffd0862920db9238ef412413636
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2019-09-15 18:20:05 -0400

    order by attendance

--- convert_to_json.rb
+++ convert_to_json.rb
@@ -5,18 +5,23 @@ require 'chroma'
 require 'json'
 
 basic_data = CSV.read "basic_con_data.csv"
-organized_data = Hash.new { |hash, key| hash[key] = Hash.new { |hash, key| hash[key] = 0 } }
+organized_data = Array.new { |hash, key| hash[key] = Hash.new { |hash, key| hash[key] = 0 } }
 basic_data[1..-1].each do |curr_con|
-	organized_data[curr_con[0]][:location] = Hash.new
-	organized_data[curr_con[0]][:location][:lat] = curr_con[2].to_f
-	organized_data[curr_con[0]][:location][:lng] = curr_con[3].to_f
-	organized_data[curr_con[0]][:attendance] = curr_con[4].to_i
+	curr_organizer = Hash.new { |hash, key| hash[key] = 0 }
+	curr_organizer[:name] = curr_con[0]
+	curr_organizer[:location] = Hash.new { |hash, key| hash[key] = 0 }
+	curr_organizer[:location][:lat] = curr_con[2].to_f
+	curr_organizer[:location][:lng] = curr_con[3].to_f
+	curr_organizer[:attendance] = curr_con[4].to_i
 	exact_date = Date.strptime(curr_con[5], "%m/%d/%Y")
 	hue = exact_date.yday / 365.0 * 360.0
-	organized_data[curr_con[0]][:date] = exact_date.strftime "%d %b %Y"
-	organized_data[curr_con[0]][:color] = "hsv(#{hue}, 1, 1)".paint.to_hex
-	organized_data[curr_con[0]][:fclr] = (curr_con[6].to_i == 1)
+	curr_organizer[:date] = exact_date.strftime "%d %b %Y"
+	curr_organizer[:color] = "hsv(#{hue}, 1, 1)".paint.to_hex
+	curr_organizer[:fclr] = (curr_con[6].to_i == 1)
+	organized_data << curr_organizer
 end
+
+organized_data.sort { |a,b| a[:attendance] <=> b[:attendance] }
 
 File.open("map_data.json", "w") do |fout|
    fout.syswrite JSON.pretty_generate(organized_data)
