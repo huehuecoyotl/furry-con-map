ab98be66c7a594dae744655139ba46c4966081f6
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2019-09-15 17:16:15 -0400

    Make dates look more natural

--- convert_to_json.rb
+++ convert_to_json.rb
@@ -11,9 +11,9 @@ basic_data[1..-1].each do |curr_con|
 	organized_data[curr_con[0]][:location][:lat] = curr_con[2].to_f
 	organized_data[curr_con[0]][:location][:lng] = curr_con[3].to_f
 	organized_data[curr_con[0]][:attendance] = curr_con[4].to_i
-	organized_data[curr_con[0]][:date] = curr_con[5]
 	exact_date = Date.strptime(curr_con[5], "%m/%d/%Y")
 	hue = exact_date.yday / 365.0 * 360.0
+	organized_data[curr_con[0]][:date] = exact_date.strftime "%d %b %Y"
 	organized_data[curr_con[0]][:color] = "hsv(#{hue}, 1, 1)".paint.to_hex
 end
 
