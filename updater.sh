2e5551d3cce4959b1ac82e4f0d58116882c93a58
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2020-07-25 02:21:57 -0500

    updater should actually update now

--- updater.sh
+++ updater.sh
@@ -2,9 +2,8 @@
 
 cd /home/ubuntu/map_muncher
 
-if git diff --quiet
+if !(git pull | grep "Already up to date.")
 then
-    git pull
     ./convert_to_json.rb
 fi
 cp ./map_data.json /home/ubuntu/coyotl/source/public/data/
